784fcd6aaebcedfacdbb4141b3cc0c0e62ec9dba
keside <wilsonezeala@yahoo.com>
2020-10-28 00:31:52 +0100

    port settings

--- index.js
+++ index.js
@@ -1,58 +1,54 @@
 // const { request } = require('express');
 // const { response } = require('express');
-require("dotenv").config()
-console.log(process.env)
-const fetch = require("node-fetch")
+require('dotenv').config();
+console.log(process.env);
+const fetch = require('node-fetch');
 const express = require('express');
 const Datastore = require('nedb');
 const app = express();
-const database = new Datastore('database.db')
+const database = new Datastore('database.db');
 database.loadDatabase();
 const apiKey = process.env.apikey;
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=> console.log('listening at 3000'));
+app.listen(port, () => console.log('listening at ' + port));
 // to serve up a webpage
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/api', function(req, res){
+app.post('/api', function (req, res) {
     const timestamp = Date.now();
     data = req.body;
     data.timestamp = timestamp;
-   database.insert(data)
-    res.json(data)
-
-})
-app.get('/api', function(req, res){
-    database.find({},(err,data)=>{
-        if(err){
+    database.insert(data);
+    res.json(data);
+});
+app.get('/api', function (req, res) {
+    database.find({}, (err, data) => {
+        if (err) {
             res.end();
-            return
+            return;
         }
-        res.json(data)
-    })
-})
+        res.json(data);
+    });
+});
 
+app.get(`/weather/:latlon`, async (req, res) => {
+    const latlon = req.params.latlon.split(',');
+    console.log(latlon);
+    const lat = latlon[0];
+    const lon = latlon[1];
+    const weather_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const weather_response = await fetch(weather_url);
+    const weather_json = await weather_response.json();
 
+    const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
+    const aq_response = await fetch(aq_url);
+    const aq_json = await aq_response.json();
 
-
-        app.get(`/weather/:latlon`, async (req,res)=>{
-            const latlon = req.params.latlon.split(",");
-            console.log(latlon);
-            const lat = latlon[0];
-            const lon = latlon[1];
-            const weather_url =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-            const weather_response = await fetch(weather_url)
-            const weather_json = await weather_response.json();
-            
-
-            const aq_url =`https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`
-            const aq_response = await fetch(aq_url)
-            const aq_json = await aq_response.json();
-
-            const data = {
-                weather : weather_json,
-                air_quality: aq_json
-        }
-            res.json( data)
-        })
+    const data = {
+        weather: weather_json,
+        air_quality: aq_json,
+    };
+    res.json(data);
+});
--- public/log.js
+++ public/log.js
@@ -2,23 +2,21 @@ async function getData() {
     const response = await fetch("/api");
     const data = await response.json();
 
-    data.forEach(el =>{
-       const marker =  L.marker([el.lat, el.lon]).addTo(mymap)
+    data.forEach((el) => {
+        const marker = L.marker([el.lat, el.lon]).addTo(mymap);
 
         const txt = `
         the weather here is
         ${el.weather.weather.description} with a temperture of
         ${el.weather.main.temp} degrees fahrenheit
         ${el.aq}
-        `
+        `;
 
         marker.bindPopup(txt);
-    })
+    });
 
     console.log(data);
 }
-
-
 
 //mapping
 const mymap = L.map("map").setView([0, 0], 1);
@@ -28,6 +26,4 @@ const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(mymap);
 
-
-            
-              getData();
+getData();
